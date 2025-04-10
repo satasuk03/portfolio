d81deb60fdbe9402c6b260901b550f274cbabd00
Satasuk Viparksinlapin <satasuk03@gmail.com>
2025-04-10 23:12:08 +0700

    update portfolio-page

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,6 +1,13 @@
 "use client";
 import React from "react";
+import MyHeroSection from "@/ui/shared/MyHeroSection";
 
 export default function Portfolio() {
-  return <div>In Development</div>;
+  return (
+    <>
+      <div className="flex h-screen w-screen items-center justify-center overflow-y-auto">
+        <MyHeroSection />
+      </div>
+    </>
+  );
 }
