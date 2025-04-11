1f470eee49d75d8b271fbf7def9a686c5a3c1e44
Satasuk Viparksinlapin <satasuk03@gmail.com>
2025-04-11 20:41:18 +0700

    fix type

--- src/hooks/useOutsideClick.tsx
+++ src/hooks/useOutsideClick.tsx
@@ -2,11 +2,11 @@ import React, { useEffect } from "react";
 
 export const useOutsideClick = (
   ref: React.RefObject<HTMLDivElement | null>,
-  callback: Function
+  callback: (event: MouseEvent | TouchEvent) => void
 ) => {
   useEffect(() => {
-    const listener = (event: any) => {
-      if (!ref || !ref.current || ref.current.contains(event.target)) {
+    const listener = (event: MouseEvent | TouchEvent) => {
+      if (!ref || !ref.current || ref.current.contains(event.target as Node)) {
         return;
       }
       callback(event);
