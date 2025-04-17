deca80e944d70b215927f601f42e7f269ef95ea2
Satasuk Viparksinlapin <satasuk03@gmail.com>
2025-04-17 22:05:06 +0700

    add instagram link

--- src/app/portfolio/layout.tsx
+++ src/app/portfolio/layout.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import NavBar from "@/ui/shared/NavBar";
+import { SiInstagram } from "@icons-pack/react-simple-icons";
 
 export default function Layout({
   children,
@@ -13,6 +14,11 @@ export default function Layout({
       <div className="md:mx-[15%] mx-4 my-4 max-w-[1200px] bg-terminal-black-800/80 rounded-lg">
         {children}
       </div>
+      <footer className="flex justify-center items-center gap-4 my-4 sm:my-8 text-secondary/80">
+        <a href="https://www.instagram.com/zezethewanderer/">
+          <SiInstagram className="w-6 h-6" />
+        </a>
+      </footer>
     </>
   );
 }
