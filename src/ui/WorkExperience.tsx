3dc39b081e5178542640b97bfd03bfd5c2f1c51c
Satasuk Viparksinlapin <satasuk03@gmail.com>
2025-04-18 21:20:28 +0700

    update ui to match other pages

--- src/ui/WorkExperience.tsx
+++ src/ui/WorkExperience.tsx
@@ -43,28 +43,27 @@ export function WorkExperience() {
   ];
   return (
     <div className="relative w-full overflow-clip font-abel h-fit">
-      <h2 className="sm:text-2xl text-lg font-medium font-poppins text-secondary/80">
+      <h2 className="sm:text-2xl text-lg font-medium font-poppins text-secondary/80 sm:mb-4 mb-2">
         Work Experience
       </h2>
-
       {data.map((item, index) => {
         return (
           <div
             key={index}
-            className="collapse collapse-arrow text-terminal-white/80"
+            className="collapse rounded-none collapse-arrow text-terminal-white/80"
           >
             <input
               type="radio"
               name="my-accordion-2"
               defaultChecked={index === 0}
             />
-            <div className="collapse-title font-semibold text-terminal-white">
+            <div className="collapse-title p-0 font-semibold text-terminal-white">
               {item.title}
               <div className="text-xs text-terminal-white/80">
                 {item.subtitle}
               </div>
             </div>
-            <div className="collapse-content text-sm">{item.content}</div>
+            <div className="collapse-content text-sm p-0">{item.content}</div>
           </div>
         );
       })}
